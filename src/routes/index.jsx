394f3b86bfc739d921a2ef8d84dc7346ba03ba20
Siddharth Patel <opensiddhu993@gmail.com>
2024-04-17 14:05:55 +0530

    comingsoon page added

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,21 +2,23 @@ import { useRoutes } from "react-router-dom";
 import Main from "../layouts/main";
 import Token from "../pages/Token";
 import Token1 from "../pages/Token1";
+import ComingSoon from "../pages/ComingSoon";
+
 const Router = () => {
   return useRoutes([
     {
         path:'/',
-        element:<Main />,
-        children:[
-            {
-              index:true,
-              element:<Token />
-            },
-            {
-              path:'/token1',
-              element:<Token1 />
-            }
-        ]
+        element:<ComingSoon />,
+        // children:[
+        //     {
+        //       index:true,
+        //       element:<Token />
+        //     },
+        //     {
+        //       path:'/token1',
+        //       element:<Token1 />
+        //     }
+        // ]
     }
   ])
 }
