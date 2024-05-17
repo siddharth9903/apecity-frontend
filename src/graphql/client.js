ae5190943640d924532b1673518cb9fec698d010
Siddharth Patel <opensiddhu993@gmail.com>
2024-05-17 19:39:53 +0530

    graphql auth added

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,9 +1,33 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+});
+
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: `${import.meta.env.VITE_AUTH_TOKEN}`, // Use your token here
+        }
+    }
+});
 
 const client = new ApolloClient({
-    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
-    connectToDevTools: true, 
+    connectToDevTools: true,
 });
 
 export default client;
+
+// import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+// const client = new ApolloClient({
+//     uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+//     cache: new InMemoryCache(),
+//     connectToDevTools: true, 
+// });
+
+// export default client;
