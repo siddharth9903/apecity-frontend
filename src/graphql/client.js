86ad80af401262cda28fcded2034ac4fda8d859b
Siddharth Patel <opensiddhu993@gmail.com>
2024-05-20 09:11:45 +0530

    graphql client modification for dev

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -6,12 +6,13 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
+    const token = import.meta.env.VITE_AUTH_TOKEN;
     return {
         headers: {
             ...headers,
-            authorization: `${import.meta.env.VITE_AUTH_TOKEN}`, // Use your token here
-        }
-    }
+            ...(token ? { authorization: `${token}` } : {}),
+        },
+    };
 });
 
 const client = new ApolloClient({
@@ -22,12 +23,39 @@ const client = new ApolloClient({
 
 export default client;
 
-// import { ApolloClient, InMemoryCache } from '@apollo/client';
+// import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+
+// const httpLink = createHttpLink({
+//     uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//     return {
+//         headers: {
+//             ...headers,
+//             authorization: `${import.meta.env.VITE_AUTH_TOKEN}`, // Use your token here
+//         }
+//     }
+// });
 
 // const client = new ApolloClient({
-//     uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+//     link: authLink.concat(httpLink),
 //     cache: new InMemoryCache(),
-//     connectToDevTools: true, 
+//     connectToDevTools: true,
 // });
 
 // export default client;
+
+
+
+
+// // import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+// // const client = new ApolloClient({
+// //     uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+// //     cache: new InMemoryCache(),
+// //     connectToDevTools: true, 
+// // });
+
+// // export default client;
