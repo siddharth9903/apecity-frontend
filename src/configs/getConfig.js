7337d99edca2649bbce2daeaaed24055d2a3ca2b
Siddharth Patel <opensiddhu993@gmail.com>
2024-06-27 16:14:14 +0530

    baseIdTenderly configured

--- src/configs/getConfig.js
+++ src/configs/getConfig.js
@@ -12,27 +12,31 @@ import {
 } from "./tenderly.config"
 
 
-const baseConfig = {
-    rpcUrl: baseRpcUrl,
-    chains: baseConfigChains,
-    config: baseProviderConfig,
-    defaultChain: baseDefaultChain,
-    graphqlEndpoint: 'https://ffd04q3km9.execute-api.us-east-2.amazonaws.com/subgraphs/name/APE',
-    ipfsEndpoint: 'https://ipfs.apecity.fun',
-    authToken: 'secretToken',
-    apeFactoryAddress: '0xb4FBc25204d26C4a937F4CBa67087F70B21bb6c5'
-}
+// const baseConfig = {
+//     rpcUrl: baseRpcUrl,
+//     chains: baseConfigChains,
+//     config: baseProviderConfig,
+//     defaultChain: baseDefaultChain,
+//     graphqlEndpoint: 'https://ffd04q3km9.execute-api.us-east-2.amazonaws.com/subgraphs/name/APE',
+//     ipfsEndpoint: 'https://ipfs.apecity.fun',
+//     authToken: 'secretToken',
+//     apeFactoryAddress: '0xb4FBc25204d26C4a937F4CBa67087F70B21bb6c5'
+// }
 
 const baseIdTenderlyConfig = {
     rpcUrl: baseIdTenderlyRpcUrl,
     chains: baseIdTenderlyConfigChains,
     config: baseIdTenderlyProviderConfig,
     defaultChain: baseIdTenderlyDefaultChain,
-    graphqlEndpoint: 'https://e7jijlguod.execute-api.us-east-1.amazonaws.com/subgraphs/name/APE',
+    graphqlEndpoint: 'https://tend.apecity.xyz/subgraphs/name/APE',
+    wsEndpoint: 'wss://tend.apecity.xyz/subgraphs/name/APE?type=ws',
     ipfsEndpoint: 'https://ipfs.apecity.fun',
     authToken: 'secretToken',
     apeFactoryAddress: '0x7722B77e691ceA11047f030f1b128432A1a6FfCA'
 }
+
+const baseConfig = baseIdTenderlyConfig
+
 
 export const getConfig = () => {
     const network = import.meta.env.VITE_NETWORK
@@ -58,6 +62,7 @@ export const getConfig = () => {
         return {
             ...defaultConfig,
             graphqlEndpoint: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+            wsEndpoint: import.meta.env.VITE_WS_ENDPOINT,
             ipfsEndpoint: import.meta.env.VITE_IPFS_ENDPOINT,
             authToken: import.meta.env.VITE_AUTH_TOKEN
         }
--- src/graphql/client.js
+++ src/graphql/client.js
@@ -7,22 +7,12 @@ import { getConfig } from '../configs/getConfig';
 // Create an HTTP link
 const httpLink = new HttpLink({
     uri: getConfig().graphqlEndpoint,
-    // uri: 'http://ecs-api-graph-tenderly-lb-1919492898.us-east-1.elb.amazonaws.com/subgraphs/name/APE?type=api'
 });
 
 // Create a WebSocket link
 const wsLink = new WebSocketLink({
-    uri: 'ws://localhost:8001/subgraphs/name/APE',
-    // uri: 'ws://ecs-ws-graph-tenderly-lb-247375954.us-east-1.elb.amazonaws.com/subgraphs/name/APE',
-    // uri: 'ws://ecs-api-graph-tenderly-lb-1919492898.us-east-1.elb.amazonaws.com/subgraphs/name/APE?type=ws',
-    // uri: 'https://ws.apecity.fun/subgraphs/name/APE',
-    // uri: 'http://184.72.85.174:8001/subgraphs/name/APE',
-    // uri: 'ws://184.72.85.174:8001/subgraphs/name/APE',
-    // uri: 'https://ws.apecity.fun/subgraphs/name/APE',
-    // uri: 'https://2bhu85569j.execute-api.us-east-1.amazonaws.com/subgraphs/name/APE',
-    // uri: 'http://2bhu85569j.execute-api.us-east-1.amazonaws.com/subgraphs/name/APE',
-    // uri: 'https://2bhu85569j.execute-api.us-east-1.amazonaws.com',
-    
+    uri: getConfig().wsEndpoint,
+    // uri: 'wss://tend.apecity.xyz/subgraphs/name/APE?type=ws',
     options: {
         reconnect: true,
         connectionParams: {
